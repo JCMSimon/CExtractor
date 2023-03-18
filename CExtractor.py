1fc05494499f68c76876cfa8b841148da9e288ab
Simon <simon1240.se@gmail.com>
2023-03-18 19:29:00 +0100

    stuff

--- CExtractor.py
+++ CExtractor.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException,NoSuchElementException,NoSuchFrameException
 import requests
+import random
 import pathlib
 
 CurrentDir = pathlib.Path(__file__).parent.absolute()
@@ -36,8 +37,8 @@ while True:
 	except TimeoutException:
 		prompt = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".rc-imageselect-desc")))
 	print("Prompt:", str(prompt.text).replace("\n"," "))
-	if "once there are none left" in prompt.text:
-		input("THIS SHOULD BE AN ANNOYING ONE (Images fade out and new ones appear)")
+	# if "once there are none left" in prompt.text:
+	# 	input("THIS SHOULD BE AN ANNOYING ONE (Images fade out and new ones appear)")
 	try:
 		image = driver.find_elements(By.TAG_NAME,"img")[0]
 		url = image.get_attribute("src")
@@ -49,7 +50,7 @@ while True:
 		input(f"GRID ({e})")
 	print(f"Grid:{grid[0]}x{grid[1]}")
 	with requests.get(url,stream=True) as url:
-			with open(f"{CurrentDir}/output.png", "wb") as file:
+			with open(f"{CurrentDir}/{random.randint(0,55555)}output.png", "wb") as file:
 				shutil.copyfileobj(url.raw, file)
 	# except (TimeoutException,NoSuchElementException,NoSuchFrameException,Exception) as e:
 	# 	print("##########################")
